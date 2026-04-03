da41631503f1e11964975b8f0092f5c99655dd8d
HerdiansyahMaulanAhmad <herdiansyahmaulan879@gmail.com>
2026-04-03 22:36:51 +0700

    edit tautan

--- resources/views/aspirations/index.blade.php
+++ resources/views/aspirations/index.blade.php
@@ -423,7 +423,7 @@
                     <p>Sistem Online Suara Aspirasi Mahasiswa</p>
                 </div>
                 <div class="col-md-3">
-                    <h5 class="mb-3">Tautan</h5>
+                    <h5 class="mb-3">Tautan 1139813 akamowd</h5>
                     <ul class="list-unstyled">
                         <li><a href="#" class="text-white text-decoration-none">Aspirasi</a></li>
                         <li><a href="#" class="text-white text-decoration-none">Tentang</a></li>
@@ -433,7 +433,7 @@
                     </ul>
                 </div>
                 <div class="col-md-3">
-                    <h5 class="mb-3">Hubungi Kami 12434214</h5>
+                    <h5 class="mb-3">Hubungi Kami</h5>
                     <ul class="list-unstyled">
                         <li><i class="fab fa-instagram me-2"></i><a href="#" class="text-white text-decoration-none">@hmti.unimus</a></li>
                         <li><i class="fab fa-instagram me-2"></i><a href="#" class="text-white text-decoration-none">@advokastrat.hmti</a></li>
