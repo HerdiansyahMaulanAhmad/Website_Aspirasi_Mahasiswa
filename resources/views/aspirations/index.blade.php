7df2e52b289bacd00fe1f87cbd413239e5e20d39
HerdiansyahMaulanAhmad <herdiansyahmaulan879@gmail.com>
2026-04-04 21:08:01 +0700

    dosen ti

--- resources/views/aspirations/index.blade.php
+++ resources/views/aspirations/index.blade.php
@@ -420,10 +420,10 @@
             <div class="row">
                 <div class="col-md-6">
                     <h5 class="mb-3"><i class="fas fa-comment-dots me-2"></i>SAMA-TI</h5>
-                    <p>Sistem Online Suara Aspirasi Mahasiswa</p>
+                    <p>Sistem Online Suara Aspirasi Mahasiswa cikidot dot dot doen ti kurang dalam mengajar</p>
                 </div>
                 <div class="col-md-3">
-                    <h5 class="mb-3">Tautan 1139813 akamowd</h5>
+                    <h5 class="mb-3">Tautan</h5>
                     <ul class="list-unstyled">
                         <li><a href="#" class="text-white text-decoration-none">Aspirasi</a></li>
                         <li><a href="#" class="text-white text-decoration-none">Tentang</a></li>
